e8dd904a72861e470ac4b589b7ff286582cc0f9b
tronbodya <tronbodya@gmail.com>
2023-09-27 11:18:23 +0300

    local k8s

--- frontend/Program.cs
+++ frontend/Program.cs
@@ -5,6 +5,8 @@ using GloboTicket.Frontend.Services.ShoppingBasket;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Configuration.AddEnvironmentVariables();
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
