d352a8fd6afcfcc3ef3a4c06a513bd92cd08ff26
tronbodya <tronbodya@gmail.com>
2023-09-30 19:32:18 +0300

    security improve

--- catalog/Program.cs
+++ catalog/Program.cs
@@ -5,8 +5,6 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Configuration.AddEnvironmentVariables();
-
 // Add services to the container.
 builder.Services.AddHttpClient();
 builder.Services.AddControllers();
